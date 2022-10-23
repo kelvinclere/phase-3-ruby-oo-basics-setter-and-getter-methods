e89ae267be552b8d99196c1426ad31240a5fd090
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-10-23 10:07:42 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
-class Dog
 
+class Dog
+    #setter method
+    def name=(name)
+        @name=name
+    end
+    
+    def breed=(breed)
+        @breed=breed
+        end
+
+    #getter method
+    def name
+        @name
 end
+
+ def breed
+    @breed
+ end
+end
+
